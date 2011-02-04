031d6cfd048df5298a393953a1a583fcb0517db5
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2011-02-04 16:35:01 +0000

    icon_exprs handle for plone 4 compatibility

--- monet/calendar/event/Extensions/install.py
+++ monet/calendar/event/Extensions/install.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
 
+from Products.CMFPlone.utils import getFSVersionTuple
+
+def install(portal, reinstall=False):
+    setup_tool = portal.portal_setup
+    setup_tool.setBaselineContext('profile-monet.calendar.event:uninstall')
+    setup_tool.runAllImportStepsFromProfile('profile-monet.calendar.event:uninstall')
+    if getFSVersionTuple()[0]>=4:
+        unregisterIcon(portal)
+
 def uninstall(portal, reinstall=False):
     if not reinstall:
         setup_tool = portal.portal_setup
-        setup_tool.setImportContext('profile-monet.calendar.event:uninstall')
+        setup_tool.setBaselineContext('profile-monet.calendar.event:uninstall')
+        setup_tool.runAllImportStepsFromProfile('profile-monet.calendar.event:uninstall')
         setup_tool.runAllImportSteps()
+    if getFSVersionTuple()[0]>=4:
+        unregisterIcon(portal)
+
+def unregisterIcon(portal):
+    """Remove icon expression from Event type"""
+    log = portal.plone_log
+    portal_types = portal.portal_types
+    t = portal_types.getTypeInfo("Event")
+    #t.icon_expr = ''
+    t.content_icon = ''
+    t.manage_changeProperties(content_icon='', icon_expr='')
+    log("Removing icon type info")
--- monet/calendar/event/exportimport.py
+++ monet/calendar/event/exportimport.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from Products.CMFCore.utils import getToolByName
 
 # Properties are defined here, because if they are defined in propertiestool.xml,
